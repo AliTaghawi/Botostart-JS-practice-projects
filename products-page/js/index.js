1a19cc8eb89f497f1112d39a1be2198400a8eece
Ali Taghawi <marshaltaghawi@gmail.com>
2023-11-21 20:15:55 +0330

    <products page project> adding filter by name feacher event

--- products-page/js/index.js
+++ products-page/js/index.js
@@ -1,1 +1,16 @@
-console.log("hellooo there");
+const searchByNameInput = document.querySelector("#searchByName input");
+const products = document.querySelectorAll(".product-card");
+
+const nameFilterHandler = (event) => {
+  const searchValue = event.target.value.toLowerCase().trim();
+  products.forEach((product) => {
+    const productName = product.querySelector("p").innerHTML.toLowerCase();
+    if (productName.includes(searchValue)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+};
+
+searchByNameInput.addEventListener("keyup", nameFilterHandler);
