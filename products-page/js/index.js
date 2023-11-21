8dba71c58604db65d0b4001595d0de72be5d3275
Ali Taghawi <marshaltaghawi@gmail.com>
2023-11-21 21:10:29 +0330

    <products page project> adding filter by category feacher

--- products-page/js/index.js
+++ products-page/js/index.js
@@ -1,5 +1,6 @@
 const searchByNameInput = document.querySelector("#searchByName input");
 const products = document.querySelectorAll(".product-card");
+const categoryButtons = document.querySelectorAll(".categorys button");
 
 const nameFilterHandler = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
@@ -13,4 +14,30 @@ const nameFilterHandler = (event) => {
   });
 };
 
+const categoryFilterHandler = (event) => {
+  // changing styles of buttons
+  categoryButtons.forEach((button) => {
+    button === event.target
+      ? button.classList.add("active")
+      : button.classList.remove("active");
+  });
+  // filtering process
+  const category = event.target.innerText.toLowerCase();
+  products.forEach((product) => {
+    if (category === "all") {
+      product.style.display = "block";
+    } else {  
+      const productCategory = product.dataset.category;
+      productCategory === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
+
+
+
 searchByNameInput.addEventListener("keyup", nameFilterHandler);
+categoryButtons.forEach((button) => {
+  button.addEventListener("click", categoryFilterHandler);
+});
