54a3dc55da9090deedb6c6ccecdb14313da01f9b
Ali Taghawi <marshaltaghawi@gmail.com>
2023-11-24 15:57:26 +0330

    <todo app project> creating function to generating Id for todo

--- todo-app/js/app.js
+++ todo-app/js/app.js
@@ -1,21 +1,32 @@
-const taskInput = document.getElementById("task-input")
-const dateInput = document.getElementById("date-input")
-const addButton = document.getElementById("add-todo")
-const todos = []
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addButton = document.getElementById("add-todo");
+const todos = [];
+
+const generatId = () => {
+  return (
+    "ID" +
+    (
+      Math.trunc(Math.random() * Date.now()).toString() +
+      Math.trunc(Math.random() * Date.now()).toString()
+    ).slice(0, 16)
+  );
+};
 
 const addHandler = (event) => {
-  event.preventDefault()
-  const task = taskInput.value
-  const date = dateInput.value
+  event.preventDefault();
+  const task = taskInput.value;
+  const date = dateInput.value;
   const todo = {
+    id: generatId(),
     status: false,
     task,
     date,
-  }
-  todos.push(todo)
-  taskInput.value = ""
-  dateInput.value = ""
+  };
+  todos.push(todo);
+  taskInput.value = "";
+  dateInput.value = "";
   console.log(todos)
-}
+};
 
-addButton.addEventListener("click", addHandler)
+addButton.addEventListener("click", addHandler);
